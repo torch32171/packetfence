8b3ef4a5db20f25742b8f2c42a77547a9f338b40
James Rouzier <jrouzier@inverse.ca>
2016-01-06 12:23:53 -0500

    New config file in packetfence conf/traffic_shaping.conf

--- lib/pf/file_paths.pm
+++ lib/pf/file_paths.pm
@@ -98,6 +98,7 @@ our (
     $switch_control_dir,
     $pfmon_config_file, $pfmon_default_config_file,
     $switch_filters_config_file,
+    $traffic_shaping_config_file,
 );
 
 BEGIN {
@@ -170,6 +171,7 @@ BEGIN {
         $switch_control_dir
         $pfmon_config_file $pfmon_default_config_file
         $switch_filters_config_file
+        $traffic_shaping_config_file
     );
 }
 
@@ -210,6 +212,7 @@ $log_config_file    = catfile($conf_dir, "log.conf");
 $provisioning_config_file = catfile($conf_dir, 'provisioning.conf');
 $device_registration_config_file = catfile($conf_dir,"device_registration.conf");
 $pki_provider_config_file  = catfile($conf_dir,"pki_provider.conf");
+$traffic_shaping_config_file  = catfile($conf_dir,"pfitemconfig.conf");
 
 $network_config_file    = catfile($conf_dir, "networks.conf");
 $switches_config_file   = catfile($conf_dir, "switches.conf");
@@ -305,6 +308,7 @@ $captiveportal_default_profile_templates_path = catdir ($captiveportal_profile_t
     $dns_filters_config_file,
     $pfmon_config_file,
     $switch_filters_config_file,
+    $traffic_shaping_config_file,
 );
 
 $pffilter_socket_path = catfile($var_dir, "run/pffilter.sock");
